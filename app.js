c04b53da53931e832b685f0622d5a91476663b2e
Frank <chenmaofrank1996@gmail.com>
2021-04-28 16:12:36 +0300

    write engine code into the APP.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+
+app.engine('html', require('express-art-template'));
+
+app.set('view options', {
+    debug: process.env.NODE_ENV !== 'development'
+});
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'html');
+
 
 const port = 3000;
 
